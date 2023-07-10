7d91d9690723e815ca702c148f020dcdcd622672
mshof <mshofwan.dev@gmail.com>
2023-07-10 19:46:47 +0700

    [Section 30] Create Hero Entity

---
+++ app/src/main/java/me/project/borutoapp/domain/models/Hero.kt
@@ -0,0 +1,21 @@
+package me.project.borutoapp.domain.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import me.project.borutoapp.utils.Constant
+
+@Entity(tableName = Constant.DatabaseConst.TB_HERO)
+data class Hero(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val name: String,
+    val image: String,
+    val about: String,
+    val rating: Double,
+    val power: Int,
+    val month: String,
+    val day: String,
+    val family: List<String>,
+    val abilities: List<String>,
+    val natureTypes: List<String>
+)
--- app/src/main/java/me/project/borutoapp/utils/Constant.kt
+++ app/src/main/java/me/project/borutoapp/utils/Constant.kt
@@ -2,4 +2,9 @@ package me.project.borutoapp.utils
 
 object Constant {
     const val ID = "id"
+
+    object DatabaseConst{
+        const val DB_HERO = "db_hero"
+        const val TB_HERO = "tb_hero"
+    }
 }
