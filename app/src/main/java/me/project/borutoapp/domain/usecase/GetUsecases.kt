827aa70a7fb92361722c8dc3a7085661a174a4b7
mshof <mshofwan.dev@gmail.com>
2023-07-13 17:35:05 +0700

    [Section 41] Create Repository & First Use Cases - my version

--- app/src/main/java/me/project/borutoapp/domain/usecase/GetUsecases.kt
+++
@@ -1,6 +0,0 @@
-package me.project.borutoapp.domain.usecase
-
-data class GetUsecases(
-    val readOnBoardingUseCase: ReadOnBoardingUseCase,
-    val saveOnBoardingUseCase: SaveOnBoardingUseCase
-)
