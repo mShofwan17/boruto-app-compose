878379e7c5e153fde2827221b21bd89c3ca13e89
mshof <mshofwan.dev@gmail.com>
2023-07-13 14:17:01 +0700

    [Section 36] Animate Splash Screen & Apply StatusBar Color

--- app/src/main/java/me/project/borutoapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/me/project/borutoapp/presentation/screens/splash/SplashScreen.kt
@@ -2,14 +2,19 @@ package me.project.borutoapp.presentation.screens.splash
 
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -17,16 +22,24 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import me.project.borutoapp.R
-import me.project.borutoapp.ui.theme.Purple40
-import me.project.borutoapp.ui.theme.Purple80
 
 @Composable
 fun SplashScreen(navHostController: NavHostController) {
-    Splash()
+    val rotate = remember { Animatable(0f) }
+    LaunchedEffect(key1 = true) {
+        rotate.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+    }
+    Splash(rotate = rotate.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(rotate: Float) {
     val backgroundColor = if (isSystemInDarkTheme()) listOf(Color.Black, Color.DarkGray)
     else listOf(Color.Blue, Color.Cyan)
 
@@ -39,7 +52,7 @@ fun Splash() {
         Image(
             painter = painterResource(id = R.drawable.logo),
             contentDescription = stringResource(id = R.string.app_logo),
-            //modifier = Modifier.align(Alignment.Center)
+            modifier = Modifier.rotate(degrees = rotate)
         )
     }
 }
@@ -47,11 +60,11 @@ fun Splash() {
 @Composable
 @Preview
 private fun Preview() {
-    Splash()
+    Splash(rotate = 0f)
 }
 
 @Composable
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 private fun DarkReview() {
-    Splash()
+    Splash(rotate = 0f)
 }
